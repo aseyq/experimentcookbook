60718abb6263fa2df87765d22b6297fd24a0e39c
Ali Seyhun Saral <aseyq@posteo.net>
2025-01-30 17:01:12 +0100

    quarto autorun

---
+++ install_quarto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the Quarto version
+QUARTO_VERSION="1.6.40"
+
+# Download and install Quarto
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
+sudo dpkg -i quarto-${QUARTO_VERSION}-linux-amd64.deb
+
+# Verify installation
+quarto --version
